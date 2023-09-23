6696e1a48d0ab4c1667f24f630ece22e5d0f4668
D Dheeraj <dhiraajroy123@gmail.com>
2023-09-23 16:38:26 +0530

    added ROAMIO watermark

--- frontend/components/Map.js
+++ frontend/components/Map.js
@@ -20,30 +20,33 @@ export default function Map({ center, zoom, showMarker, data }) {
 
     return (
         center ? (
-            <MapContainer center={center} zoom={zoom} zoomControl={false} className="absolute z-0 w-screen h-screen overflow-hidden">
-                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-                <ZoomControl position='bottomleft' />
-                {showMarker && <Marker position={center} icon={icon1} />}
-                {data && data.map((e, index) => (
-                    <div key={index}>
-                        <Marker key={index} position={[e.latitude, e.longitude]} icon={icon2}>
-                            <Popup>
-                                <div className="flex flex-col gap-2">
-                                    <h1 className="text-xl font-bold">{e.name}</h1>
-                                    <details>
-                                        <summary>More information</summary>
-                                        {Object.entries(e).map(([key, value], i) => (
-                                            <p key={i}>{key}: {value ? value : 'NaN'}</p>
-                                        ))}
-                                    </details>
-                                    <button className="flex gap-2 justify-center items-center" onClick={() => window.open(`https://www.openstreetmap.org/directions?engine=graphhopper_car&to=${e.latitude}%2C${e.longitude}`, '_blank')}><FaDirections />Direction</button>
-                                </div>
-                            </Popup>
-                        </Marker>
-                        <Marker position={[e.latitude, e.longitude]} icon={new L.divIcon({ html: `<span>${e.name}</span>`, className: 'font-semibold' })} />
-                    </div>
-                ))}
-            </MapContainer>
+            <>
+                <MapContainer center={center} zoom={zoom} zoomControl={false} className="absolute z-0 w-screen h-screen overflow-hidden">
+                    <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                    <ZoomControl position='bottomleft' />
+                    {showMarker && <Marker position={center} icon={icon1} />}
+                    {data && data.map((e, index) => (
+                        <div key={index}>
+                            <Marker key={index} position={[e.latitude, e.longitude]} icon={icon2}>
+                                <Popup>
+                                    <div className="flex flex-col gap-2">
+                                        <h1 className="text-xl font-bold">{e.name}</h1>
+                                        <details>
+                                            <summary>More information</summary>
+                                            {Object.entries(e).map(([key, value], i) => (
+                                                <p key={i}>{key}: {value ? value : 'NaN'}</p>
+                                            ))}
+                                        </details>
+                                        <button className="flex gap-2 justify-center items-center" onClick={() => window.open(`https://www.openstreetmap.org/directions?engine=graphhopper_car&to=${e.latitude}%2C${e.longitude}`, '_blank')}><FaDirections />Direction</button>
+                                    </div>
+                                </Popup>
+                            </Marker>
+                            <Marker position={[e.latitude, e.longitude]} icon={new L.divIcon({ html: `<span>${e.name}</span>`, className: 'font-semibold' })} />
+                        </div>
+                    ))}
+                </MapContainer>
+                <h1 className="fixed bottom-5 right-5 z-10 text-black/50 text-3xl font-semibold">ROAMIO</h1>
+            </>
         ) : ''
     )
 }

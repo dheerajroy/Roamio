05db255f003607b9429f0a34390bd7bbc3acba6c
D Dheeraj <dhiraajroy123@gmail.com>
2023-09-26 15:40:25 +0530

    adding message while loading

--- frontend/components/Loading.js
+++ frontend/components/Loading.js
@@ -1,7 +1,14 @@
+import { useEffect, useState } from "react"
+
 export default function Loading() {
+    const [messageState, setMessageState] = useState(false)
+    useEffect(() => {
+        setTimeout(() => setMessageState(true), 10000)
+    }, [])
     return (
-        <div className="fixed w-screen h-screen top-0 grid place-items-center">
+        <div className="fixed w-screen h-screen top-0 flex flex-col gap-2 justify-center items-center">
             <span className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-black"></span>
+            {messageState ? <p className="p-2">Might take 1-2 mins as this platform is hosted on render free tier, the server takes time to start.</p> : ""}
         </div>
     )
 }
